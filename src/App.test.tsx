ac254c8a82ca4f564057ad4fecddf7c0f73ab2f8
Jocelyn Facchini <jocelyn.facchini@codurance.com>
2025-06-20 15:14:56 +0100

    Revert test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import {getCompanyData} from "./api/companyData";
-import {getUserData} from "./api/userData";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders hello world', () => {
+test("renders hello world", () => {
   render(<App />);
-  const heading = screen.getByRole('heading', { name: /hello world/i });
+  const heading = screen.getByRole("heading", { name: /hello world/i });
   expect(heading).toBeInTheDocument();
 });
-
-test('', () => {
-  getUserData().then(console.log);
-})
