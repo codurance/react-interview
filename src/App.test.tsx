d340350402ff8f597b51ee21279e4a9e88013506
Jocelyn Facchini <jocelyn.facchini@codurance.com>
2025-06-20 15:22:03 +0100

    Adapt prettier to current style

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders hello world", () => {
+test('renders hello world', () => {
   render(<App />);
-  const heading = screen.getByRole("heading", { name: /hello world/i });
+  const heading = screen.getByRole('heading', { name: /hello world/i });
   expect(heading).toBeInTheDocument();
 });
